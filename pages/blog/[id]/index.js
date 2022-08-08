655defdd508cc33f2cecb56e85e3cd1ab21a0c34
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-08 13:51:54 +0200

    skeleton for dynamic blog route, rename blog posts

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -1,15 +1,28 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const BlogPost = () => {
 
   const router = useRouter()
   const { id } = router.query
 
-  return <p>Post: {id}</p>
+  return (
+    <>
+      <h3>This is post {id}</h3>
+      <Link href="/">Go back</Link>
+    </>
+  )
 }
 
-export const getServerSideProps = () => {
+export const getStaticProps = async (context) => {
   return {props: {}}
+}
+
+export const getStaticPaths = async (context) => {
+  return {
+    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+    fallback: false, // can also be true or 'blocking'
+  }
 }
 
 export default BlogPost
