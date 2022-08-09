572f3fbf2af5fcf76102f41cce6e01654c6725d2
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-09 10:44:11 +0200

    slight styling changes to index and layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-export default function Home() {
+import Link from "next/link"
+import { getSortedPostsData } from "../lib/posts"
+import Head from "next/head"
+import styles from '../styles/index.module.css'
+
+export default function Home({posts}) {
   return (
     <div>
-      <h1>Hej hopp i lingonskogen</h1>
+      <Head>
+        <title>Max Kopra</title>
+      </Head>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, ipsa!</h2>
+      <section>
+        <h2 className={styles.blogtitle}>Recent posts</h2>
+      </section>
     </div>
   )
+}
+
+export async function getStaticProps(){
+  const data = getSortedPostsData()
+  console.log(data)
+  return {
+    props: {
+      data
+    }
+  }
 }
