849c15a09305cec5ac1694c011093fe537953324
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-09 09:04:17 +0200

    able to render the titles now

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -2,29 +2,33 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { getSortedPostsData, getAllPostIds, getPostData } from '../../../lib/posts'
 
-const BlogPost = () => {
+const BlogPost = ({postData}) => {
 
   const router = useRouter()
   const { id } = router.query
 
   return (
     <>
-      <h3>This is post {id}</h3>
+      <h3>{postData.title}</h3>
       <Link href="/">Go back</Link>
     </>
   )
 }
 
-export const getStaticProps = async (context) => {
-  console.log(getSortedPostsData())
-  console.log(getPostData())
-  return {props: {}}
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
 }
 
-export const getStaticPaths = async (context) => {
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
   return {
-    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
-    fallback: false, // can also be true or 'blocking'
+    props: {
+      postData
+    }
   }
 }
 
