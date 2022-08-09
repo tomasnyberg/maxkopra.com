1182cd39bc90fa686450f68b2e1326f17d831a09
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-09 10:58:21 +0200

    mostly implement the index page

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,28 @@ export default function Home({posts}) {
       </Head>
       <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, ipsa!</h2>
       <section>
-        <h2 className={styles.blogtitle}>Recent posts</h2>
+        <h2 className={styles.blogheader}>Recent posts</h2>
+        {posts.map(({title, subtitle, date, id}) => {
+          return (
+            <div className={styles.blog} key={id}>
+              <Link href={`/blog/${id}`}>
+                <h3 className={styles.blogtitle}>{title}</h3>
+              </Link>
+              <h4 className={styles.blogsubtitle}>{subtitle}</h4>
+              <h5 className={styles.blogdate}>{date}</h5>
+            </div>
+          )
+        })}
       </section>
     </div>
   )
 }
 
 export async function getStaticProps(){
-  const data = getSortedPostsData()
-  console.log(data)
+  const posts = getSortedPostsData()
   return {
     props: {
-      data
+      posts 
     }
   }
 }
