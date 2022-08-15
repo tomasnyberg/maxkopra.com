22be4cdb3d93b92201b670861ec9dde2f0ac4b34
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-15 09:22:35 +0200

    working concept of darkmode toggle, need to fix index.js dark/light styles and get a nice button, also footer

--- components/Layout.js
+++ components/Layout.js
@@ -3,21 +3,31 @@ import Footer from './Footer'
 import Meta from './Meta'
 import LayoutStyles from '../styles/Layout.module.css'
 import '../styles/Layout.module.css'
-import { useState } from 'react'
+import { createContext, useState } from 'react'
+
+export const darkmodeContext = createContext(false)
 
 const Layout = ({children}) => {
   const [darkmode, setdarkmode] = useState(false)
+
+  const toggleDarkmode = () => {
+    setdarkmode(!darkmode)
+  }
+
   return (
-    <div className={`${LayoutStyles.container} ${darkmode ? LayoutStyles.dark:LayoutStyles.light}`}>
-      <Meta></Meta>
-      <Nav></Nav>
-      <div>
-        <main className={LayoutStyles.main}>
-          {children}
-        </main>
-          <Footer/>
+    // Create a provider for the darkmodeContext
+    <darkmodeContext.Provider value={{darkmode, toggleDarkmode}}>
+      <div className={`${LayoutStyles.container} ${darkmode ? LayoutStyles.dark:LayoutStyles.light}`}>
+        <Meta></Meta>
+        <Nav></Nav>
+        <div>
+          <main className={LayoutStyles.main}>
+            {children}
+          </main>
+            <Footer/>
+        </div>
       </div>
-    </div>
+    </darkmodeContext.Provider>
   )
 }
 
--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,16 @@
 import styles from '../styles/Nav.module.css'
 import Link from 'next/link'
+import { useContext } from 'react'
+import { darkmodeContext } from './Layout'
 
 const Nav = () => {
+
+  const { darkmode, toggleDarkmode } = useContext(darkmodeContext)
+
   return (
     <>
-      <div className={styles.nav}>
+      <div className={`${styles.nav} ${darkmode ? styles.dark:styles.light}`}>
+        <button onClick={toggleDarkmode}>asdf</button>
         <ul>
           <Link  href="/about">
             <li className={styles.link}>
