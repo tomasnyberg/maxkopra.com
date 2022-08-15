766a596503a05401c85eb3d62960f5c336ee5712
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-15 11:33:20 +0200

    add image, put it into the about.js file

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 import Meta from '../components/Meta'
 import styles from '../styles/about.module.css'
+import Image from 'next/image'
+import maxPic from '../public/max2.PNG'
 
 const about = () => {
   return (
     <>
       <Meta title="About"></Meta>
       <h1>About me</h1>
+      <Image className={styles.pfp} width="200px" height="200px" src={maxPic}></Image>
       <p>Some more text</p>
       <Link href="/">Go back</Link>
     </>
