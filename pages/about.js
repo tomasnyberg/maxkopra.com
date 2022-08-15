33553542e8072dd3ea0e48171adfe66fb2a23ea6
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-15 11:26:37 +0200

    change fonts

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import Meta from '../components/Meta'
+import styles from '../styles/about.module.css'
 
 const about = () => {
   return (
     <>
       <Meta title="About"></Meta>
-      <h3>This is the about page</h3>
+      <h1>About me</h1>
       <p>Some more text</p>
       <Link href="/">Go back</Link>
     </>
