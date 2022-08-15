758fa594db95b3b174d3bfccd55d0d3c26d2d50f
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-15 09:00:51 +0200

    concept for how to implement darkmode, adding class to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import Nav from './Nav'
-import Header from './Header'
 import Footer from './Footer'
 import Meta from './Meta'
 import LayoutStyles from '../styles/Layout.module.css'
+import '../styles/Layout.module.css'
+import { useState } from 'react'
 
 const Layout = ({children}) => {
+  const [darkmode, setdarkmode] = useState(false)
   return (
-    <div className={LayoutStyles.container}>
+    <div className={`${LayoutStyles.container} ${darkmode ? LayoutStyles.dark:LayoutStyles.light}`}>
       <Meta></Meta>
       <Nav></Nav>
       <div>
