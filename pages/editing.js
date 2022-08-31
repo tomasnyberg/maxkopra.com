56a08d857971b3b3c745abfabd11fb6c921a5431
tomasnyberg <tomas.nyberg7335@gmail.com>
2022-08-31 14:52:47 +0200

    add transition to header and footer, and text for editing page

--- pages/editing.js
+++ pages/editing.js
@@ -7,7 +7,9 @@ const books = () => {
       <Meta title="Editing"></Meta>
       <h1>Editing services</h1>
       <h2>(coming soon!)</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores eligendi reiciendis modi sint enim suscipit adipisci consequatur, quibusdam ad. Placeat dolore dolorum illo eaque natus perspiciatis. Dicta distinctio repellat tempora, maxime laboriosam molestias voluptas velit minima facilis, tempore temporibus non recusandae. Voluptates, exercitationem ipsam. Iste, quia architecto nemo accusamus asperiores doloremque reiciendis, fuga hic perferendis saepe debitis minus optio maxime earum repellat delectus. Repellat sint earum quod adipisci ea accusamus quas maxime iusto magni officia aliquid esse tempora est minus corrupti, possimus dicta dolorum consequuntur hic! Debitis quod corporis est exercitationem laborum minima molestiae, tenetur non expedita commodi sequi quibusdam!</p>
+      <p>Tired of vague feedback on your drafts? Want to take your writing to the next level through a systematic editing approach?</p>
+      <div></div>
+      <p>Kopra will soon be offering editing services to help you take your story to the next level!</p>
       <Link href="/">Go back</Link>
     </>
   )
